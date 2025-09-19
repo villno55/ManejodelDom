e1dbf160f560f65d12918acfb0a0b899736e0e10
villno55 <adsotrabajos24@gmail.com>
2025-09-18 19:39:16 -0500

    Update script.js

--- ejer1/script.js
+++ ejer1/script.js
@@ -1,1 +1,8 @@
+const divInfo = document.getElementById("info");
+    console.log("Texto del div info:", divInfo.textContent);
+    
+    const detalle = document.querySelector(".detalle");
+    detalle.textContent = "Este es un detalle modificado";
 
+    const parrafos = document.querySelectorAll("p");
+    parrafos.forEach(p => p.style.backgroundColor = "lightblue");
