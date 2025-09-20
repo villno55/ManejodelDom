2308671133bfa073eb95186819b8f87847f73a22
villno55 <adsotrabajos24@gmail.com>
2025-09-20 16:10:51 -0500

    Update script.js

--- ejer2/script.js
+++ ejer2/script.js
@@ -1,1 +1,7 @@
+const enlace = document.getElementsByTagName("a")[0];
+    enlace.href = "https://www.google.com";
 
+    const titulo = document.querySelector(".titulo");
+    titulo.textContent = "Título Modificado";
+
+    document.body.style.backgroundColor = "lightyellow";
