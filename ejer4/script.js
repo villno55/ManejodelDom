9c4aed34a270dca19efa4653cc827b691acda30e
villno55 <adsotrabajos24@gmail.com>
2025-09-20 16:41:42 -0500

    Corregido

--- ejer4/script.js
+++ ejer4/script.js
@@ -1,1 +1,7 @@
+    const nuevoDiv = document.createElement("div");
+    nuevoDiv.textContent = "Soy un nuevo div";
 
+    const viejoDiv = document.querySelector("div");
+    viejoDiv.parentNode.replaceChild(nuevoDiv, viejoDiv);
+
+    console.log("Div reemplazado correctamente");
