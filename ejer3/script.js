44665a0ad9993772683e48cbaa50abca487e04cf
villno55 <adsotrabajos24@gmail.com>
2025-09-20 16:40:25 -0500

    Update script.js

--- ejer3/script.js
+++ ejer3/script.js
@@ -3,7 +3,7 @@
     paNew.textContent = "párrafo creado dinámicamente";
     document.body.appendChild(paNew);
 
-    const btnBorrar = document.querySelector("#btn-borrar");
+    const btnBorrar = document.querySelector("#eliminar");
     btnBorrar.addEventListener("click", () => {
       const primP = document.querySelector("p");
       if (primP) {
