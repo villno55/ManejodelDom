b7d2e1059f44b7118f9d9e000aea6e299c5de803
villno55 <adsotrabajos24@gmail.com>
2025-09-21 10:49:31 -0500

    Update script.js

--- ejer5/script.js
+++ ejer5/script.js
@@ -1,1 +1,13 @@
+const btnClickMe = document.getElementById("clickMe");
+    const btnRemove = document.getElementById("removeEvent");
 
+    function mostrarAlerta() {
+      alert("¡Botón clickeado!");
+    }
+
+    btnClickMe.addEventListener("click", mostrarAlerta);
+
+    btnRemove.addEventListener("click", () => {
+      btnClickMe.removeEventListener("click", mostrarAlerta);
+      alert("Evento eliminado del botón Click Me");
+    });
