73f497dbc56f23a39dbba3c72d0e7a6d520e78be
villno55 <adsotrabajos24@gmail.com>
2025-09-21 10:51:11 -0500

    Update script.js

--- ejer6/script.js
+++ ejer6/script.js
@@ -1,1 +1,36 @@
+const input = document.getElementById("inputTarea");
+    const btnAgregar = document.getElementById("btnAgregar");
+    const lista = document.getElementById("listaTareas");
 
+    btnAgregar.addEventListener("click", () => {
+      if (input.value.trim() === "") 
+        
+      const divTarea = document.createElement("div");
+      divTarea.classList.add("tarea");
+
+      const texto = document.createElement("span");
+      texto.textContent = input.value;
+
+      const check = document.createElement("span");
+      check.textContent = "✔";
+      check.classList.add("icono", "check");
+      check.addEventListener("click", () => {
+        check.classList.toggle("realizada");
+      });
+
+      const eliminar = document.createElement("span");
+      eliminar.textContent = "✖";
+      eliminar.classList.add("icono", "delete");
+      eliminar.addEventListener("click", () => {
+        if (confirm("¿Seguro que deseas eliminar esta tarea?")) {
+          divTarea.remove();
+        }
+      });
+
+      divTarea.appendChild(texto);
+      divTarea.appendChild(check);
+      divTarea.appendChild(eliminar);
+      lista.appendChild(divTarea);
+
+      input.value = "";
+    });
